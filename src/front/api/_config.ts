0598e3020c66ff23ff9b3183a35ee2cb2a3dc921
anonymze <anodevfr@gmail.com>
2025-07-04 12:13:49 +0200

    add: react + ts config

--- src/front/api/_config.ts
+++ src/front/api/_config.ts
@@ -1,14 +1,18 @@
 import axios from "axios";
 
+const ORIGIN_WEB = "simply-life-app://web";
+
 /**
  * @description WITHCREDENTIALS PROPERTY IS NEEDED TO SEND / STORE THE COOKIES
  */
 export const api = axios.create({
   baseURL: process.env.BUN_PUBLIC_API_URL || "",
+  timeout: 15 * 1000, // 15 seconds
   responseType: "json",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
+    "X-Origin": ORIGIN_WEB,
   },
   withCredentials: true,
 });
